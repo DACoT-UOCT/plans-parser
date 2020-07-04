9bbba3c92d881501e9a67529228811474aecce79
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-07-03 20:53:57 -0400

    Refactor plans.py script to reduce Cognitive Complexity

--- parser.py
+++ parser.py
@@ -3,48 +3,50 @@ import json
 import argparse
 
 # [python:S4784]: There is no risk for a ReDoS since the input text to evaluate is not provided by users
-first_re = re.compile(r'^Plan\s+(?P<id>\d+)\s(?P<junction>J\d{6}).*(?P<cycle>CY\d{3})\s(?P<phases>[A-Z0-9\s,]+)$')
-second_re = re.compile(r'[A-Z]{1,2}\s\d{1,3}')
+re_plan = re.compile(r'^Plan\s+(?P<id>\d+)\s(?P<junction>J\d{6}).*(?P<cycle>CY\d{3})\s(?P<phases>[A-Z0-9\s,!]+)$')
+re_phases = re.compile(r'[A-Z]{1,2}\s\d{1,3}')
 
-def read_plans(file):
+def build_phases(re_phases_match):
+    initial_format = [{i.split()[0]: int(i.split()[1])} for i in re_phases_match]
+    phases_dict = {}
+    for phase in initial_format:
+        phases_dict.update(phase)
+    # Map letter to numbers in  phases ids
+    result = {}
+    for k in phases_dict.keys():
+        if len(k) > 1:
+            kn = int(''.join([str(ord(i) - 64) for i in list(k)]))
+            result[kn] = phases_dict[k]
+        else:
+            kn = ord(k) - 64
+            result[kn] = phases_dict[k]
+    return result
+
+def parse_plan(text):
+    plan = re_plan.match(text)
+    if not plan:
+        return False, None
+    phases = re_phases.findall(plan.group('phases'))
+    if not phases:
+        return False, None
+    formatted_phases = build_phases(phases)
+    plan_timings = {
+        'cycle': int(plan.group('cycle').split('Y')[1]),
+        'system_start': formatted_phases
+    }
+    return True, (plan.group('junction'), plan.group('id'), plan_timings)
+
+def parse_plans_file(fpath, encoding='iso-8859-1'):
     plans = {}
-    with open(file, encoding='iso-8859-1') as fplans:
-        count = 0
+    with open(fpath, encoding=encoding) as fplans:
         for line in fplans.readlines():
-            count += 1
             line = line.strip()
-            match_junction = first_re.match(line)
-            if not match_junction:
-                print("WARN: Line [ {} ] didn't match first regex".format(line))
-                continue
-            match_phases = second_re.findall(match_junction.group('phases'))
-            if not match_phases:
-                print("WARN: Phases string [ {} ] didn't match regex".format(match_junction.group('phases')))
-                continue
-            join_phases = {}
-            match_phases = [{i.split()[0]: int(i.split()[1])} for i in match_phases]
-            phases_dict = {}
-            for m in match_phases:
-                join_phases.update(m)
-            for k in join_phases.keys():
-                if len(k) > 1:
-                    nk = int(''.join([str(ord(i) - 64) for i in list(k)]))
-                    phases_dict[nk] = join_phases[k]
-                else:
-                    nk = ord(k) - 64
-                    phases_dict[nk] = join_phases[k]
-            if not match_junction.group('junction') in plans:
-                plans[match_junction.group('junction')] = {}
-                plans[match_junction.group('junction')][match_junction.group('id')] = {
-                    'cycle': int(match_junction.group('cycle').split('Y')[1]),
-                    'system_start': phases_dict
-                }
-            else:
-                plans[match_junction.group('junction')][match_junction.group('id')] = {
-                    'cycle': int(match_junction.group('cycle').split('Y')[1]),
-                    'system_start': phases_dict
-                }
-        print('{} lines parsed'.format(count))
+            ok, plan = parse_plan(line)
+            if ok:
+                junct, plan_id, plan_timings = plan
+                if not junct in plans:
+                    plans[junct] = {}
+                plans[junct][plan_id] = plan_timings
     return plans
 
 if __name__ == "__main__":
@@ -54,7 +56,7 @@ if __name__ == "__main__":
     args_parser.add_argument('--junc', type=str, help='Junction to verify')
     args = args_parser.parse_args()
     # Parse
-    plans = read_plans(args.input)
+    plans = parse_plans_file(args.input)
     print('{} junctions parsed'.format(len(plans.keys())))
     if args.junc:
         print('\nExtracted data for {}:'.format(args.junc))
