4dc213948300c6674109a95078d871c564caeb15
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-07-05 22:03:09 -0400

    Add initial UTCPlanParser test

--- tests/test_plans_parser.py
+++ tests/test_plans_parser.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from parser.utc_plan_parser import UTCPlanParser
 
@@ -5,7 +6,9 @@ class TestPlanParser(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestPlanParser, self).__init__(*args, **kwargs)
         self.parser = UTCPlanParser()
+        self.input = os.path.join(os.path.dirname(__file__), 'plans.txt')
 
-    def test_example(self):
-        self.assertEqual(1, 1)
+    def test_parse_file(self):
+        plans = self.parser.parse_plans_file(self.input)
+        self.assertEqual(len(plans.keys()), 4)
     
