4d998c00d4cbfa81ca6b97b64aa8f5009fa67461
camilo alberto parra ruiz <caparra@alumnos.inf.utfsm.cl>
2020-07-14 11:43:19 -0400

    quitado parametro hour en funcion parse_program y variable hour cambiada a propiedad de la clase

--- dacot_parser/utc_program_parser.py
+++ dacot_parser/utc_program_parser.py
@@ -5,25 +5,26 @@ class UTCProgramParser():
         # [python:S4784]: There is no risk for a ReDoS since the input text to evaluate is not provided by users
         self.__re_hour = re.compile(r'(?P<hour>\d{2}:\d{2}:\d{2})\s+[A-Z]+\s+\d{3,4}\s+\(.*PLANES.*\)$')
         self.__re_plan = re.compile(r'(?P<hour>(\d{2}:\d{2}:\d{2})?)(\d{3})?\s+PLAN\s+(?P<junction>J\d{6})\s+(?P<plan>(\d+|[A-Z]{1,2}))\s+TIMETABLE$')
+        self.__hour = '0'
 
-    def parse_program(self,text,hour):
+    def parse_program(self,text):
+        match_hour_line =  self.__re_hour.match(text)
+        if match_hour_line:   
+            self.__hour = match_hour_line.group('hour')
+            return False, None
         plan = self.__re_plan.match(text)
         if not plan:
             return False, None
         if plan.group('hour') != '':
-            hour = [plan.group('hour')]
-        return True, (plan.group('junction'), hour, plan.group('plan'))
+            self.__hour = [plan.group('hour')]
+        return True, (plan.group('junction'), self.__hour, plan.group('plan'))
 
     def parse_program_file(self, fpath, day, encoding='iso-8859-1'):
         programs = {}
-        hour = '0'
         with open(fpath, encoding=encoding) as fplans:
             for line in fplans.readlines():
                 line = line.strip()
-                match_hour_line =  self.__re_hour.match(line)
-                if match_hour_line:   
-                    hour = match_hour_line.group('hour')
-                ok, plan = self.parse_program(line,hour)
+                ok, plan = self.parse_program(line)
                 if ok:
                     junct, f_hour, f_plan= plan
                     if not junct in programs:
--- tests/test_program_parser.py
+++ tests/test_program_parser.py
@@ -16,6 +16,6 @@ class TestProgramParser(unittest.TestCase):
         self.assertEqual(programs['J099181']['S'][1][1], 'XS')
     
     def test_parse_invalid_program(self):
-        self.assertFalse(self.parser.parse_program('00:01:00   ACAS 321 (-00:01 INICIALIZA SPECIAL FACLTY, SABADO)','00:00:00')[0])
-        self.assertFalse(self.parser.parse_program('001   PLAN A099180 11 TIMETABLE','00:00:00')[0])
-        self.assertFalse(self.parser.parse_program('002   PLAN J0991 S TIMETABLE','00:00:00')[0])
+        self.assertFalse(self.parser.parse_program('00:01:00   ACAS 321 (-00:01 INICIALIZA SPECIAL FACLTY, SABADO)')[0])
+        self.assertFalse(self.parser.parse_program('001   PLAN A099180 11 TIMETABLE')[0])
+        self.assertFalse(self.parser.parse_program('002   PLAN J0991 S TIMETABLE')[0])
