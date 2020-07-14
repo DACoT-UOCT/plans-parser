343329ea41c6fbbd18713fff818734feeb2141f0
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-07-13 22:11:54 -0400

    Store commands and results in a dictionary instead of a list

--- dacot_parser/telnet_command_executor.py
+++ dacot_parser/telnet_command_executor.py
@@ -7,18 +7,22 @@ class TelnetCommandExecutor():
         self.__target_host = host
         self.__target_port = port
         self.__conn_timeout = connection_timeout
-        self.__reads_outputs = []
+        self.__reads_outputs = {}
         self.__command_history = []
+        self.__current_command = 'init'
 
-    def command(self, command):
-        self.__command_history.append(command)
-        self.__commands.put(lambda **kwargs: self.__command_impl(command, **kwargs))
+    def command(self, identifier, command):
+        self.__command_history.append(identifier)
+        item = (identifier, lambda **kwargs: self.__command_impl(command, **kwargs))
+        self.__commands.put(item)
 
     def read_lines(self, encoding='ascii', line_ending=b'\n'):
-        self.__commands.put(lambda **kwargs: self.__read_lines_impl(encoding, line_ending, **kwargs))
+        item = ('read_lines', lambda **kwargs: self.__read_lines_impl(encoding, line_ending, **kwargs))
+        self.__commands.put(item)
 
     def read_until(self, text, timeout=None, encoding='ascii'):
-        self.__commands.put(lambda **kwargs: self.__read_until_impl(text, timeout, encoding, **kwargs))
+        item = ('read_until', lambda **kwargs: self.__read_until_impl(text, timeout, encoding, **kwargs))
+        self.__commands.put(item)
 
     def get_results(self):
         return self.__reads_outputs
@@ -34,7 +38,7 @@ class TelnetCommandExecutor():
             if l == b'':
                 break
             lines.append(l.decode(encoding).rstrip())
-        self.__reads_outputs.append(lines)
+        return lines
 
     def __command_impl(self, command, **kwargs):
         telnet = kwargs['telnet']
@@ -44,7 +48,7 @@ class TelnetCommandExecutor():
     def __read_until_impl(self, text, timeout, encoding, **kwargs):
         telnet = kwargs['telnet']
         out = telnet.read_until(bytes(text, encoding=encoding), timeout)
-        self.__reads_outputs.append(out.decode(encoding))
+        return out.decode(encoding)
 
     def reset(self):
         self.__reads_outputs.clear()
@@ -55,4 +59,12 @@ class TelnetCommandExecutor():
         # TODO: Add debug messages, method try/catch and return status code
         with telnetlib.Telnet(self.__target_host, self.__target_port, self.__conn_timeout) as tn_client:
             while not self.__commands.empty():
-                self.__commands.get()(telnet=tn_client)
+                cmd = self.__commands.get()
+                if 'read' in cmd[0]:
+                    if not self.__current_command in self.__reads_outputs:
+                        self.__reads_outputs[self.__current_command] = []
+                    output = cmd[1](telnet=tn_client)
+                    self.__reads_outputs[self.__current_command].append(output)
+                else:
+                    self.__current_command = cmd[0]
+                    cmd[1](telnet=tn_client)
