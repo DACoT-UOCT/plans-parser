dfeae066a8499716a679b078051ac4f166ee58ae
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-07-14 02:26:42 -0400

    Remove unused regex in UTCProgramParser

--- dacot_parser/utc_program_parser.py
+++ dacot_parser/utc_program_parser.py
@@ -5,7 +5,6 @@ class UTCProgramParser():
         # [python:S4784]: There is no risk for a ReDoS since the input text to evaluate is not provided by users
         self.__re_hour = re.compile(r'(?P<hour>\d{2}:\d{2}:\d{2})\s+[A-Z]+\s+\d{3,4}\s+\(.*PLANES.*\)$')
         self.__re_plan = re.compile(r'(?P<hour>(\d{2}:\d{2}:\d{2})?)(\d{3})?\s+PLAN\s+(?P<junction>J\d{6})\s+(?P<plan>(\d+|[A-Z]{1,2}))\s+TIMETABLE$')
-        self.__re_extract_hour = re.compile(r'\d{2}:\d{2}:\d{2}')
 
     def parse_program(self,text,hour):
         plan = self.__re_plan.match(text)
@@ -14,7 +13,6 @@ class UTCProgramParser():
         if plan.group('hour') != '':
             hour = [plan.group('hour')]
         return True, (plan.group('junction'), hour, plan.group('plan'))
-
 
     def parse_program_file(self, fpath, day, encoding='iso-8859-1'):
         programs = {}
