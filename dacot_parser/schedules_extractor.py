e516f4b3d999c473c0fc4af84fe31009b7256748
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-07-14 02:18:34 -0400

    Update SchedulesExtractor with changes in the TelnetCommandExecutor

--- dacot_parser/schedules_extractor.py
+++ dacot_parser/schedules_extractor.py
@@ -5,39 +5,45 @@ from dacot_parser.utc_plan_parser import UTCPlanParser
 from dacot_parser.utc_program_parser import UTCProgramParser
 
 class SchedulesExtractor():
-    def __init__(self):
-        self.__utc_host = os.environ['DACOT_UOCT_CTRL_HOST']
-        self.__utc_user = os.environ['DACOT_UOCT_CTRL_USER']
-        self.__utc_passwd = os.environ['DACOT_UOCT_CTRL_PASS']
+    def __init__(self, host, user, passwd):
+        self.__utc_host = host
+        self.__utc_user = user
+        self.__utc_passwd = passwd
         self.__executor = TelnetCommandExecutor(self.__utc_host)
         self.__plans_parser = UTCPlanParser()
-        self.__program_parser = UTCProgramParser()
+        # self.__program_parser = UTCProgramParser()
         self.__re_ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|[0-9]|\[[0-?]*[ -/]*[@-~])|\r|\n')
 
     def build_schedules(self):
-        all_plans = self.__parse_plans('A000000')
-        print(all_plans)
+        self.__executor.reset()
+        #plans, failed_plans = self.__parse_plans('A000000')
+        return self.__parse_plans('A000000')
 
+    def get_results(self):
+        return self.__executor.get_results()
+
+    # TODO: Check the case when there is no slots available in the system
     def __login(self):
         self.__executor.read_until('Username:', 15)
-        self.__executor.command(self.__utc_user)
+        self.__executor.command('login-user', self.__utc_user)
         self.__executor.read_until('Password:', 15)
-        self.__executor.command(self.__utc_passwd)
+        self.__executor.command('login-pass', self.__utc_passwd)
         self.__executor.read_lines(encoding='iso-8859-1')
 
     def __logout(self):
-        self.__executor.command('ENDS')
+        self.__executor.command('end-session', 'ENDS')
 
     def __parse_plans(self, junction):
         self.__login()
-        self.__executor.command('LIPT {} TIMINGS'.format(junction))
+        self.__executor.command('get-plans', 'LIPT {} TIMINGS'.format(junction))
         self.__executor.read_lines(encoding='iso-8859-1', line_ending=b'\x1b8\x1b7')
         self.__logout()
         self.__executor.run()
-        res = self.__executor.get_results()
+        results = self.__executor.get_results()
+        system_plans = results['get-plans']
         fail = []
         plans = {}
-        for plan in res[id][1:-1]: #TODO: FIX THIS
+        for plan in system_plans[1:-1]:
             clean_plan = self.__re_ansi_escape.sub('', plan)
             if not '<BAD>' in clean_plan:
                 ok, parsed = self.__plans_parser.parse_plan(clean_plan)
