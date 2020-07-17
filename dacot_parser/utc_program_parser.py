57633d88a5ea4eab2fc56bc0622f0e4fcf5e2d09
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-07-17 01:06:52 -0400

    Update __re_hour regex and remove hour in list

--- dacot_parser/utc_program_parser.py
+++ dacot_parser/utc_program_parser.py
@@ -3,7 +3,8 @@ import re
 class UTCProgramParser():
     def __init__(self):
         # [python:S4784]: There is no risk for a ReDoS since the input text to evaluate is not provided by users
-        self.__re_hour = re.compile(r'(?P<hour>\d{2}:\d{2}:\d{2})\s+[A-Z]+\s+\d{3,4}\s+\(.*PLANES.*\)$')
+        #self.__re_hour = re.compile(r'(?P<hour>\d{2}:\d{2}:\d{2})\s+[A-Z]+\s+\d{3,4}\s+\(.*PLANES.*\)$')
+        self.__re_hour = re.compile(r'(?P<hour>\d{2}:\d{2}:\d{2}).*$')
         self.__re_plan = re.compile(r'(?P<hour>(\d{2}:\d{2}:\d{2})?)(\d{3})?\s+PLAN\s+(?P<junction>[J|A]\d{6})\s+(?P<plan>(\d+|[A-Z]{1,2}))\s+TIMETABLE$')
         self.__hour = '0'
 
@@ -16,7 +17,7 @@ class UTCProgramParser():
         if not plan:
             return False, None
         if plan.group('hour') != '':
-            self.__hour = [plan.group('hour')]
+            self.__hour = plan.group('hour')
         return True, (plan.group('junction'), self.__hour, plan.group('plan'))
 
     def parse_program_file(self, fpath, day, encoding='iso-8859-1'):
