c60f4c12888927fa6729d2108fb96e76334407cd
camilo alberto parra ruiz <caparra@alumnos.inf.utfsm.cl>
2020-07-17 16:23:48 -0400

    basic flask backend

---
+++ flask-backend/src/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+from .extensions import mongo
+
+from .junction import junction
+
+from .main2 import main2
+
+def create_app(config_object='src.settings'):
+    app = Flask(__name__)
+
+    app.config.from_object(config_object)
+
+    mongo.init_app(app)
+
+    app.register_blueprint(junction)
+
+    app.register_blueprint(main2)
+
+    return app
