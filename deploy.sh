9ba125a43b490f35fbbd1be1c754b0d2354739a0
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-07-19 18:56:22 -0400

    Update format while passing env vars to docker-compose

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 mkdir -vp deploy/mongo-data/data{1,2,3}
-docker-compose -e DOCKER_MONGO_PUBLIC_ADDR=$(curl http://checkip.amazonaws.com) up -d
+DOCKER_MONGO_PUBLIC_ADDR=$(curl http://checkip.amazonaws.com) docker-compose up -d
