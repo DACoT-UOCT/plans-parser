fc35c8ac7f05aafb6038616c9cb40e74378e26c3
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-09-11 00:36:36 -0300

    Update the seed-db script process with new mongoengine models

--- deploy/seed-db.py
+++ deploy/seed-db.py
@@ -1,8 +1,13 @@
+import os
 import pymongo
 import argparse
 import json
 import logging
-from dacot_models import OTUProgramItem
+
+from mongoengine import connect
+
+from dacot_models import OTUProgramItem, JunctionPlan, JunctionPlanPhaseValue, Junction, JunctionMeta, OTU
+from dacot_models import ExternalCompany, UOCTUser, OTUController, ChangeSet, OTUMeta
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 log = logging.getLogger('seed-db')
@@ -19,40 +24,67 @@ with open(args.input, 'r') as jdf:
 total = len(jsondata.keys())
 log.info('We have {} keys to upload'.format(total))
 
-client = pymongo.MongoClient(args.mongo)
-log.info('Getting mongo server info')
-log.info(client.server_info())
-log.info('Mongo OK')
+connect('dacot-dev', host=args.mongo)
+
+log.info('¡¡¡¡DROPPING OLD DATA!!!!')
+
+Junction.drop_collection()
+OTU.drop_collection()
+ExternalCompany.drop_collection()
+UOCTUser.drop_collection()
+OTUController.drop_collection()
+ChangeSet.drop_collection()
+
+log.info('Done dropping old collections')
+
 log.info('Building models')
+
+seed_user = UOCTUser(uid=1, full_name='DACoT Automated DB Seeder', email='dacot@santiago.uoct.cl', area='TIC', rut='00000000-0')
+seed_user = seed_user.save().reload()
+
+junctions = []
+# otus = []
+
+created_otus = {}
 
 i = 0
 for k, v in jsondata.items():
-    p = (i / total) * 100
-    log.info('Inserting {} progress {:.2f}%'.format(k, p))
-    programs = []
     i += 1
-    for day, items in v['program'].items():
-        for item in items:
-            programs.append(OTUProgramItem(day=day, time=item[0][:5], plan=item[1]))
-    break
+    p = (i / total) * 100
+    log.info('Building model for {} progress {:.2f}%'.format(k, p))
+    plans = []
+    oid = 'X' + k[1:-1] + '0'
+    if not oid in created_otus:
+        programs = []
+        for day, items in v['program'].items():
+            for item in items:
+                programs.append(OTUProgramItem(day=day, time=item[0][:5], plan=item[1]))
+        metadata = OTUMeta(version='base', status='SYSTEM', status_user=seed_user)
+        created_otus[oid] = OTU(oid=oid, program=programs, metadata=metadata)
+    for plid, plan in v['plans'].items():
+        system_start = []
+        for phid, phvalue in plan['system_start'].items():
+            system_start.append(JunctionPlanPhaseValue(phid=phid, value=phvalue))
+        plans.append(JunctionPlan(plid=plid, cycle=plan['cycle'], system_start=system_start))
+    junctions.append(Junction(jid=k, plans=plans, metadata=JunctionMeta()))
 
+log.info('Bulk inserting {} junctions, this can take a while...'.format(len(junctions)))
+junctions = Junction.objects.insert(junctions)
+log.info('Done inserting junctions')
 
-# i = 0
-# for k in jsondata:
-#     i += 1
-#     item = {
-#         '_id': k
-#     }
-#     p = (i / total) * 100
-#     item.update(jsondata[k])
-#     print('Inserting {} progress {:.2f}%'.format(k, p))
-#     try:
-#         result = client.dacot.junction.insert_one(item)
-#         print('Inserted id: {}'.format(result.inserted_id))
-#     except pymongo.errors.DuplicateKeyError as err:
-#         print('Key is duplicated, deleting and retrying')
-#         client.dacot.junction.delete_one({'_id': k})
-#         result = client.dacot.junction.insert_one(item)
-#         print('Inserted id: {}'.format(result.inserted_id))
-# 
-# print('DONE')
+log.info('Updatig OTUs with junctions references')
+i = 0
+for j in junctions:
+    i += 1
+    p = (i / total) * 100
+    oid = 'X' + j['jid'][1:-1] + '0'
+    log.info('Updating model for {} progress {:.2f}%'.format(oid, p))
+    created_otus[oid].junctions.append(j)
+
+created_otus = created_otus.values()
+
+log.info('Bulk inserting {} OTUs, this can take a while...'.format(len(created_otus)))
+created_otus = OTU.objects.insert(created_otus)
+log.info('Done inserting junctions')
+
+log.info('DONE SEEDING THE DB')
