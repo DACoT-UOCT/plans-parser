e78f3063231c0698c927a8f42aeda769c5a247af
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-09-20 22:18:53 -0300

    Add output file parameters to export-agent

--- export-agent/agent.py
+++ export-agent/agent.py
@@ -28,9 +28,11 @@ def build_schedules(from_env=False):
             log.fatal('Missing params: {}'.format(excep), exc_info=True)
             return
         log.info('Extracting data from {} with env-supplied credentials'.format(ctrl_host))
+        sout = 'schedules.json'
+        fout = 'failed.json'
     else:
         log.info('Reading command line arguments for target and credentials')
-        ctrl_host, ctrl_user, ctrl_pass = read_params()
+        ctrl_host, ctrl_user, ctrl_pass, sout, fout = read_params()
         log.info('Extracting data from {} with command line-supplied credentials'.format(ctrl_host))
     extractor = SchedulesExtractor(ctrl_host, ctrl_user, ctrl_pass, debug=True, logger=log)
     try:
@@ -39,22 +41,24 @@ def build_schedules(from_env=False):
         log.fatal('Exception ocurred in the SchedulesExtractor.build_schedules() call: {}'.format(excep), exc_info=True)
         return
     log.info('Done extracting data from control. Dumping JSON files')
-    with open('schedules.json', 'w') as fp:
+    with open(sout, 'w') as fp:
         json.dump(schedules, fp)
-    log.info('Written {} junctions in schedules.json file'.format(len(schedules)))
-    with open('failed.json', 'w') as fp:
+    log.info('Written {} junctions in {} file'.format(len(schedules), sout))
+    with open(fout, 'w') as fp:
         json.dump(failed, fp)
-    log.info('Written {} failed plans, {} failed program lines for L table, {} failed program lines for S table and {} failed program lines for D table in failed.json file'
+    log.info('Written {} failed plans, {} failed program lines for L table, {} failed program lines for S table and {} failed program lines for D table in {} file'
         .format(len(failed['failed_plans']), len(failed['failed_program_tables']['L']), len(failed['failed_program_tables']['S']),
-        len(failed['failed_program_tables']['D'])))
+        len(failed['failed_program_tables']['D']), fout))
 
 def read_params():
-    parser = argparse.ArgumentParser(description='Seed the mongo db')
+    parser = argparse.ArgumentParser(description='Extract schedules from the UOCT control system')
     parser.add_argument('host', type=str, help='UOCT control system address')
     parser.add_argument('user', type=str, help='UOCT control system user name')
     parser.add_argument('passwd', type=str, help='UOCT control system user password')
+    parser.add_argument('sout', type=str, help='output path for schedules data')
+    parser.add_argument('fout', type=str, help='output path for invalid data extracted')
     args = parser.parse_args()
-    return args.host, args.user, args.passwd
+    return args.host, args.user, args.passwd, args.sout, args.fout
 
 if __name__ == "__main__":
     global log
