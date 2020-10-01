7237c272ea0dfdf195cc359c8eca8021bbcd4129
Diego Moya <32143106+ElMoyawy@users.noreply.github.com>
2020-10-01 00:18:30 -0300

    Update models.py

--- models/models.py
+++ models/models.py
@@ -138,7 +138,7 @@ class OTUMeta(EmbeddedDocument):
     commune = StringField()
     region = StringField()
     controller = ReferenceField(OTUController)
-    observations = EmbeddedDocumentListField(Comment) # Comment returned should only send message
+    observations = StringField() #cambio por String1: EmbeddedDocumentListField(Comment) # Comment returned should only send message
     imgs = StringField() # TODO: Sprint1 only one image ## Se cambió a tipo string para Sprint1, inicialmente: ListField(FileField()) 
     pdf_data = StringField() 
     location = PointField()
