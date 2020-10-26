3628cf5dbc260786cdb295fb00d5ddd78e72a23d
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-10-26 18:45:33 -0300

    Clean app/main.py file

--- fastapi-backend/app/main.py
+++ fastapi-backend/app/main.py
@@ -1,66 +1,34 @@
-from fastapi import Depends, FastAPI, Header, HTTPException,Form, File, UploadFile
+from fastapi import Depends, FastAPI, Header, HTTPException, Form, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from mongoengine import connect
-from .routers import otu, junctions,history,users, controller_model, commune#,petitions
+from .routers import otu, junctions, actions_log, users, controller_model, commune  # ,petitions
 from . import config
 from functools import lru_cache
 import os
 from typing import List
 from fastapi.responses import HTMLResponse
 
-
-
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "*",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 
 @lru_cache()
 def get_settings():
     return config.Settings()
 
-#connect('dacot-dev', host='mongodb://54.224.251.49:30001,54.224.251.49:30002,54.224.251.49:30003/?replicaset=rsuoct')
-#connect(host='mongodb://127.0.0.1:27017/UOCT')
 connect(get_settings().mongo_db, host=get_settings().mongo_uri)
-#async def get_token_header(x_token: str = Header(...)):
- #   if x_token != "fake-super-secret-token":
-  #      raise HTTPException(status_code=400, detail="X-Token header invalid")
-
 
 app.include_router(users.router)
 app.include_router(otu.router)
 app.include_router(junctions.router)
-#app.include_router(petitions.router)
-app.include_router(history.router)
+# app.include_router(petitions.router)
+app.include_router(actions_log.router)
 app.include_router(commune.router)
 app.include_router(controller_model.router)
-#app.include_router(
-  #  items.router,
-   # prefix="/items",
-    #tags=["items"],
-    #dependencies=[Depends(get_token_header)],
-    #responses={404: {"description": "Not found"}},
-#)
 
-#======================================================================
 @app.post("/files/")
-async def create_files(files: List[bytes] = File(...),data: str= Form(...)):
-    return {"file_sizes": [len(file) for file in files],"data":data}
-
+async def create_files(files: List[bytes] = File(...), data: str = Form(...)):
+    return {"file_sizes": [len(file) for file in files], "data": data}
 
 @app.post("/uploadfiles/")
 async def create_upload_files(files: List[UploadFile] = File(...)):
