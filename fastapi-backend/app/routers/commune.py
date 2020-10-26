340c4e0369319cd03f2f4cd5d6ae5c4810f25039
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-10-26 18:44:40 -0300

    Add register_action call to commune router

--- fastapi-backend/app/routers/commune.py
+++ fastapi-backend/app/routers/commune.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 from ..models import Commune
+from .actions_log import register_action
 
 router = APIRouter()
 
 @router.get('/communes')
-async def get_communes():
+async def get_communes(background_tasks: BackgroundTasks):
     r = []
     communes = Commune.objects.all()
     for c in communes:
         r.append({c.name: c.maintainer.name})
+    register_action('Desconocido', 'Commune', 'Un usuario ha consultado la lista de comunas', background=background_tasks)
     return r
