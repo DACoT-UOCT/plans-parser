13c20af721f7607b40e0bca70130879533486f6a
camilo alberto parra ruiz <caparra@alumnos.inf.utfsm.cl>
2020-10-27 00:21:43 -0300

    user and commune crud

--- fastapi-backend/app/routers/commune.py
+++ fastapi-backend/app/routers/commune.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, BackgroundTasks, HTTPException, Form
-from ..models import Commune, User
+from ..models import Commune, User,ExternalCompany
 from pydantic import EmailStr
 from .actions_log import register_action
 import json
@@ -16,27 +16,23 @@ async def get_communes(background_tasks: BackgroundTasks):
     register_action('Desconocido', 'Commune',
                     'Un usuario ha consultado la lista de comunas', background=background_tasks)
     return r
-
-
+    
 @router.put('/edit-commune/{name}', tags=["commune"], status_code=204)
-async def edit_commune(background_tasks: BackgroundTasks, user: EmailStr, name=str, data: str = Form(...)):
+async def edit_commune(background_tasks: BackgroundTasks, user: EmailStr, empresa: EmaiStr,name: str):
     user = User.objects(email=user).first()
     # obtener empresa que me envian
-    empresa = "hola"
     if user == None:
-        raise HTTPException(status_code=404, detail="User not found", headers={
-                            "X-Error": "Usuario no encontrado"},)
-        return
+        raise HTTPException(status_code=404, detail="User not found", headers={"X-Error": "Usuario no encontrado"},)
     if user.is_admin == False:
-        raise HTTPException(status_code=403, detail="Forbidden access", headers={
-                            "X-Error": "Usuario no encontrado"},)
-    mongoRequest = Commune.objects(name=name)
-    get_compaty = empresa
+        raise HTTPException(status_code=403, detail="Forbidden access", headers={"X-Error": "Usuario no encontrado"},)
+
+    empresa = ExternalCompany.objects(email=empresa).first()
+    if empresa == None:
+        raise HTTPException(status_code=404, detail="Company not found", headers={"X-Error": "Empresa no encontrada"},)
+    mongoRequest = Commune.objects(name=name).first()
     if mongoRequest == "":
-        raise HTTPException(status_code=404, detail="Item not found", headers={
-                            "X-Error": "No Found"},)
-        return
-    mongoRequest.update(set__maintainer=empresa)  # modificar esto
+        raise HTTPException(status_code=404, detail="Item not found", headers={"X-Error": "No Found"},)
+    mongoRequest.update(set__maintainer=empresa) 
 
     background_tasks.add_task(register_action, user, context="Reject Request",
                               action="Actualización de empresa a una comuna", origin="Web")
--- fastapi-backend/app/routers/users.py
+++ fastapi-backend/app/routers/users.py
@@ -2,30 +2,37 @@ from fastapi import APIRouter, Body, Query, HTTPException,BackgroundTasks,Form
 from flask_mongoengine import MongoEngine
 from pydantic import EmailStr
 from flask_mongoengine.wtf import model_form
-from ..models import User
+from ..models import User,ExternalCompany
+import json
 from .actions_log import register_action
 from mongoengine.errors import ValidationError, NotUniqueError
 
 router = APIRouter()
 
-# @router.post("/users", tags=["users"],status_code=201)
-# async def create_user(user:  dict ,background_tasks: BackgroundTasks):
-#     a_user= "Camilo"
-#     mongoUser = models.UOCTUser.from_json(json.dumps(user))
-#     print(mongoUser)
-#     try:
-#         mongoUser.validate()
-#     except ValidationError as error:
-#         print(error)
-#         return error
-#     
-#     try:
-#         mongoUser.save()
-#     except NotUniqueError:
-#         raise HTTPException(status_code=409, detail="Duplicated item",headers={"X-Error": "There goes my error"},)
-#     background_tasks.add_task(register_action,a_user,context= "POST",component= "Sistema", origin="web")
-#     #mongoUser = mongoUser.reload()
-#     return {"Respuesta": "Usuario Creado"}
+@router.post('/users', tags=["users"],status_code=201)
+async def create_user(request: Request ,user: EmailStr,background_tasks: BackgroundTasks):
+    user = User.objects(email=user).first()
+    if user:
+        if user.is_admin:  
+            body = await request.json()
+            user = User.from_json(json.dumps(body))
+            try:
+                user.validate()
+            except ValidationError as err:
+                raise HTTPException(status_code=422, detail=str(err))
+            register_action(user, 'Requests', 'El usuario {} ha creado un usuario forma correcta'.format(
+                user), background=background_tasks)
+            return {}
+        else:
+            register_action(user, 'Requests', 'El usuario {} ha intenado crear una peticion sin autorizacion'.format(
+                user), background=background_tasks)
+            raise HTTPException(status_code=403, detail='Forbidden')
+    else:
+        register_action(user, 'Requests', 'El usuario {} ha intenado crear una peticion, pero no existe'.format(
+            user), background=background_tasks)
+        raise HTTPException(
+            status_code=404, detail='User {} not found'.format(user))
+    return {"Respuesta": "Usuario Creado"}
 
 
 #@router.get("/users/me", tags=["users"])
@@ -57,15 +64,50 @@ async def read_users(user_email: EmailStr, background_tasks: BackgroundTasks):
         register_action(user_email, 'Users', 'El usuario {} ha intenado acceder a la lista de usuarios registrados, pero no existe'.format(user_email), background=background_tasks)
         raise HTTPException(status_code=404, detail='User {} not found'.format(user_email))
 
-@router.put('/change-permission', tags=["commune"],status_code=204)
-async def edit_commune(background_tasks: BackgroundTasks,user: EmailStr ,name= str,data: str = Form(...)):
+@router.put('/edit-user/{edited_user}', tags=["users"],status_code=204)
+async def edit_user(background_tasks: BackgroundTasks,edited_user: str,user: EmailStr ,request: Request):
     user = User.objects(email= user).first()
-    #como saber si quieren dar o quitar admin
-    empresa = "hola"
+    edit_user = User.objects(email=edited_user).first()
+    if user:
+        if user.is_admin:  
+            body = await request.json()
+            if edit_user:
+                company = ExternalCompany.objects(email=body.company)
+                if company:
+                    #edit_user.update(**body) # si no funciona pasar body a diccionario
+                    edit_user.is_admin = body.is_admin
+                    edit_user.rol = body.rol
+                    edit_user.area = body.area
+                    edit_user.company = company
+                else:
+                    raise HTTPException(
+                    status_code=404, detail='Company {} not found'.format(body.company))
+            else:
+                raise HTTPException(
+            status_code=404, detail='User {} not found'.format(edited_user))
+            register_action(user, 'Requests', 'El usuario {} ha editado un usuario forma correcta'.format(
+                user), background=background_tasks)
+            return {}
+        else:
+            register_action(user, 'Requests', 'El usuario {} ha intenado crear una peticion sin autorizacion'.format(
+                user), background=background_tasks)
+            raise HTTPException(status_code=403, detail='Forbidden')
+    else:
+        register_action(user, 'Requests', 'El usuario {} ha intenado crear una peticion, pero no existe'.format(
+            user), background=background_tasks)
+        raise HTTPException(
+            status_code=404, detail='User {} not found'.format(user))
+    register_action(user, 'Users', 'El usuario {} ha cambiado los permisos del usuario {}'.format(user,edited_user), background=background_tasks)
+
+@router.delete('/delete-user/{edited_user}',tags=["users"],status_code=204)
+async def delete_user(background_tasks: BackgroundTasks,edited_user: str,user: EmailStr ,data: str = Form(...)):
+    user = User.objects(email= user).first()
     if user == None:
         raise HTTPException(status_code=404, detail="User not found",headers={"X-Error": "Usuario no encontrado"},)
-        return
     if user.is_admin == False:
         raise HTTPException(status_code=403, detail="Forbidden access",headers={"X-Error": "Usuario no encontrado"},)
-    mongoRequest = User.objects(email= usuarioobtenido)
-    mongoRequest.update(set__isadmin= Depende)
+    edit_user = User.objects(email=edited_user).first()
+    if edit_user == None:
+        raise HTTPException(status_code=404, detail="User not found",headers={"X-Error": "Usuario no encontrado"},)
+    edit_user.delete()
+    register_action(user, 'Users', 'El usuario {} ha eliminado al usuario {}'.format(user,edited_user), background=background_tasks)
