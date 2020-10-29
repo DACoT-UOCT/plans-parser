83482ae1b43ddb1e0a33c7c97073fdf92cceca79
camilo alberto parra ruiz <caparra@alumnos.inf.utfsm.cl>
2020-10-29 00:39:06 -0300

    -

--- fastapi-backend/app/routers/users.py
+++ fastapi-backend/app/routers/users.py
@@ -84,6 +84,9 @@ async def edit_user(background_tasks: BackgroundTasks,edited_user: str,user_emai
             body = await request.json()
             if edit_user:
                 if "company" in body.keys():
+                    print(body)
+                    print(body["company"]["name"])
+                    print(type(body["company"]["name"]))
                     company = ExternalCompany.objects(name=body["company"]["name"])
                     edit_user.company = company
                 edit_user.is_admin = body["is_admin"]
