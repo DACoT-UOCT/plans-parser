c12a66250484633f6af924b9ebc5348ba6707a29
camilo alberto parra ruiz <caparra@alumnos.inf.utfsm.cl>
2020-10-28 23:16:00 -0300

    convert company str to dict in create_users

--- fastapi-backend/app/routers/users.py
+++ fastapi-backend/app/routers/users.py
@@ -16,10 +16,7 @@ async def create_user(request: Request ,user_email: EmailStr,background_tasks: B
         if user.is_admin:  
             body = await request.json()
             new_user = User.from_json(json.dumps(body))
-            print(type(body))
-            print(body["company"])
-            print(type(body["company"]))
-            company = body["company"]["name"]
+            company = json.loads(body["company"])["name"]
             company = ExternalCompany.objects(name=company).first()
             new_user.company = company
             try:
