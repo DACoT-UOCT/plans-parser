c13b9de898b5a0315363b13e35fb04686471b46e
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-10-30 22:14:25 -0300

    Add mongo_db parameter to run_api.sh file

--- run-api.sh
+++ run-api.sh
@@ -1,1 +1,1 @@
-mongo_uri="$MONGO_URL" server_email="$FASTAPI_MAIL_USER" email_pass="$FASTAPI_MAIL_PASS" uvicorn --host 0.0.0.0 --port 8080 app.main:app
+mongo_db="$MONGO_DB" mongo_uri="$MONGO_URL" server_email="$FASTAPI_MAIL_USER" email_pass="$FASTAPI_MAIL_PASS" uvicorn --host 0.0.0.0 --port 8080 app.main:app
