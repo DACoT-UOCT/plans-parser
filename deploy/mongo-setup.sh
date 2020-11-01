20caedb6c94abb56abe997b75ce855c37f4ed6c4
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-11-01 20:11:55 -0300

    Update mongo-setup.sh script for testing

--- deploy/mongo-setup.sh
+++ deploy/mongo-setup.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 set -euxo pipefail
 
-MONGODB1=mongo1
-MONGODB2=mongo2
-MONGODB3=mongo3
-MONGODB_PUBLIC_ADDR=${DOCKER_MONGO_PUBLIC_ADDR}
+MONGODB1=mongodb-run-tests-rs1
+MONGODB2=mongodb-run-tests-rs2
+MONGODB3=mongodb-run-tests-rs3
+IMAGE=docker.io/library/mongo:4.0.19-xenial
+
+podman stop -i ${MONGODB1} ${MONGODB2} ${MONGODB3}
+podman rm -i ${MONGODB1} ${MONGODB2} ${MONGODB3}
+podman run -d --name ${MONGODB1} -p 27017:27017 ${IMAGE} --replSet rsdacot
+podman run -d --name ${MONGODB2} -p 27018:27017 ${IMAGE} --replSet rsdacot
+podman run -d --name ${MONGODB3} -p 27019:27017 ${IMAGE} --replSet rsdacot
 
 echo "Waiting for startup.."
 sleep 10
 
 echo SETUP.sh time now: `date +"%T" `
-mongo --host ${MONGODB1}:27017 <<EOF
+mongo --host ${MONGODB_PUBLIC_ADDR}:27017 <<EOF
 var cfg = {
-    "_id": "rsuoct",
+    "_id": "rsdacot",
     "protocolVersion": 1,
     "version": 1,
     "members": [
         {
             "_id": 0,
-            "host": "${MONGODB_PUBLIC_ADDR}:30001",
+            "host": "${MONGODB_PUBLIC_ADDR}:27017",
             "priority": 2
         },
         {
             "_id": 1,
-            "host": "${MONGODB_PUBLIC_ADDR}:30002",
+            "host": "${MONGODB_PUBLIC_ADDR}:27018",
             "priority": 0
         },
         {
             "_id": 2,
-            "host": "${MONGODB_PUBLIC_ADDR}:30003",
+            "host": "${MONGODB_PUBLIC_ADDR}:27019",
             "priority": 0
         }
-    ],settings: {chainingAllowed: true}
+    ],
+    "settings": {
+        "chainingAllowed": true
+    }
 };
 rs.initiate(cfg, { force: true });
 rs.reconfig(cfg, { force: true });
