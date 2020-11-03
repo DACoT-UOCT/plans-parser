47c9c97bbb4ecd1d4d39e78fb4eab1cf540d2b03
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-11-03 17:52:24 -0300

    Update run-api.sh script with mail settings

--- run-api.sh
+++ run-api.sh
@@ -1,1 +1,1 @@
-mongo_db="$MONGO_DB" mongo_uri="$MONGO_URL" server_email="$FASTAPI_MAIL_USER" email_pass="$FASTAPI_MAIL_PASS" uvicorn --host 0.0.0.0 --port 8080 app.main:app
+mail_enabled=$MAIL_ENABLED mail_server=$MAIL_SERVER mail_port=$MAIL_PORT mail_user=$MAIL_USER mail_pass=$MAIL_PASS mail_tls=$MAIL_TLS mail_ssl=$MAIL_SSL mongo_uri="$MONGO_URL" uvicorn --host 0.0.0.0 --port 8080 app.main:app
