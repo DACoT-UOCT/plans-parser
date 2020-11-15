a9bcdd4bd14d4ca9ef0f793f757ef512a2a6222a
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-11-15 03:31:18 -0300

    Add root-path option to run-api

--- run-api.sh
+++ run-api.sh
@@ -1,1 +1,1 @@
-mail_creation_recipients=$MAIL_CREATE_RECIPIENTS mail_enabled=$MAIL_ENABLED mail_server=$MAIL_SERVER mail_port=$MAIL_PORT mail_user=$MAIL_USER mail_pass=$MAIL_PASS mail_tls=$MAIL_TLS mail_ssl=$MAIL_SSL mongo_uri="$MONGO_URL" uvicorn --host 0.0.0.0 --port 8080 app.main:app
+mail_creation_recipients=$MAIL_CREATE_RECIPIENTS mail_enabled=$MAIL_ENABLED mail_server=$MAIL_SERVER mail_port=$MAIL_PORT mail_user=$MAIL_USER mail_pass=$MAIL_PASS mail_tls=$MAIL_TLS mail_ssl=$MAIL_SSL mongo_uri="$MONGO_URL" uvicorn --host 0.0.0.0 --port 8080 app.main:app --root-path /api/v1
