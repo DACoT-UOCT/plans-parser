0f85bd978bc702b5f8922b9c65c649fa83fb69d5
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2021-01-31 23:11:50 -0300

    Remove old users router

--- fastapi_backend/app/main.py
+++ fastapi_backend/app/main.py
@@ -18,15 +18,6 @@ app = FastAPI(
 
 app.add_route('/graphql', GraphQLApp(schema=dacot_schema))
 
-# from fastapi import Depends, FastAPI, Header, HTTPException, Form, File, UploadFile
-# from fastapi.middleware.cors import CORSMiddleware
-# #from .routers import otu, junctions, users, actions_log, controller_model, commune
-# #from .routers import change_request, external_company, google_auth, failed_plans
-# from functools import lru_cache
-# import os
-# from typing import List
-# from fastapi.responses import HTMLResponse
-#
 # openapi_tags_info = [
 #     {
 #         'name': 'Junctions',
@@ -90,7 +81,6 @@ app.add_route('/graphql', GraphQLApp(schema=dacot_schema))
 # app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 
 # app.include_router(google_auth.router)
-# app.include_router(users.router)
 # app.include_router(otu.router)
 # app.include_router(junctions.router)
 # app.include_router(change_request.router)
@@ -99,13 +89,3 @@ app.add_route('/graphql', GraphQLApp(schema=dacot_schema))
 # app.include_router(controller_model.router)
 # app.include_router(external_company.router)
 # app.include_router(failed_plans.router)
-
-
-# @app.post("/files/")
-# async def create_files(files: List[bytes] = File(...), data: str = Form(...)):
-#     return {"file_sizes": [len(file) for file in files], "data": data}
-# 
-# 
-# @app.post("/uploadfiles/")
-# async def create_upload_files(files: List[UploadFile] = File(...)):
-#     return {"filenames": [file.filename for file in files]}
