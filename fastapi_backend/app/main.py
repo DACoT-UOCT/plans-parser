effbc8e0f0ba9531e45de7f767bcae4300633270
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2021-02-03 20:05:00 -0300

    Update API description and remove unused imports

--- fastapi_backend/app/main.py
+++ fastapi_backend/app/main.py
@@ -1,4 +1,4 @@
-from .config import get_settings, build_samples_for_docs_from_db
+from .config import get_settings
 from mongoengine import connect
 from starlette.graphql import GraphQLApp
 from .graphql_schema import dacot_schema
@@ -6,14 +6,18 @@ from fastapi import FastAPI
 
 connect(host=get_settings().mongo_uri)
 
-get_settings().docs_samples = build_samples_for_docs_from_db()
+api_description = '''
+API del proyecto Datos Abiertos para el Control de Tránsito
+(DACoT) desarrollado por SpeeDevs en colaboración con la Unidad Operativa de
+Control de Tránsito (UOCT) de la región Metropolitana en el contexto de la
+XXVIII Feria de Software del Departamento de Informática en la Universidad
+Técnica Federico Santa María.
+'''
 
 app = FastAPI(
     title='DACoT API',
     version='v0.2',
-    description='API del proyecto Datos Abiertos para el Control de Tránsito (DACoT) desarrollado por SpeeDevs en colaboración con  \
-        la Unidad Operativa de Control de Tránsito (UOCT) de la región Metropolitana en el contexto de la XXVIII Feria de Software del \
-        Departamento de Informática en la Universidad Técnica Federico Santa María.'
+    description=api_description
 )
 
 app.add_route('/graphql', GraphQLApp(schema=dacot_schema))
