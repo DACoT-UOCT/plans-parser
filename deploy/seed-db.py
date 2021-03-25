d34a2aa587577c19e36e35afc3f7fb8f72b92f6e
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2021-03-24 22:34:46 -0300

    Clean seed-db script towards removing it

--- deploy/seed-db.py
+++ deploy/seed-db.py
@@ -1,146 +1,3 @@
-import re
-import os
-import csv
-import time
-import json
-import argparse
-import logging
-import datetime
-
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from bson.json_util import dumps as bson_dumps
-from mongoengine import connect
-from pymongo.operations import ReplaceOne
-from pymongo.errors import BulkWriteError
-
-from dacot_models import Commune, ExternalCompany, ControllerModel
-from dacot_models import User, ProjectMeta, OTU, Project, OTUMeta
-from dacot_models import Controller, Junction, JunctionMeta, JunctionPlan
-from dacot_models import JunctionPlanPhaseValue, OTUProgramItem, ActionsLog
-from dacot_models import PlanParseFailedMessage
-
-if os.environ.get("RUNNING_TEST"):
-
-    class log:
-        @staticmethod
-        def info(msg):
-            return
-
-    is_diff = False
-else:
-    log = None
-    is_diff = None  # FIXME: Should be a parameter, not a global variable
-
-
-def setup_logging():
-    global log
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        datefmt="%d-%b-%y %H:%M:%S",
-    )
-    log = logging.getLogger("seed-db")
-    log.setLevel(logging.INFO)
-
-
-def fast_validate_and_insert(objects, model, replace=False):
-    global log
-    log.info(
-        "[fast_insert]: Called with {} {} items".format(len(objects), model.__name__)
-    )
-    log.info("[fast_insert]: Validating objects before bulk write")
-    mongo_objs = []
-    for obj in objects:
-        obj.validate()
-        mongo_objs.append(obj.to_mongo())
-    log.info("[fast_insert]: Validation OK")
-    if replace:
-        log.info("[fast_insert]: Writing objects using bulk_write")
-        ops = []
-        for mongo_obj in mongo_objs:
-            ops.append(
-                ReplaceOne({"_id": mongo_obj.get("_id")}, mongo_obj, upsert=True)
-            )
-        insert_res = model._get_collection().bulk_write(ops)
-    else:
-        log.info("[fast_insert]: Writing objects using insert_many")
-        insert_res = model._get_collection().insert_many(mongo_objs)
-    log.info("[fast_insert]: Write command done, collecting objects from remote")
-    if replace:
-        results = model._get_collection().find(
-            {"_id": {"$in": [x.get("_id") for x in mongo_objs]}}
-        )
-    else:
-        results = model._get_collection().find(
-            {"_id": {"$in": insert_res.inserted_ids}}
-        )
-    mongo_objs.clear()
-    for r in results:
-        mongo_objs.append(model.from_json(bson_dumps(r)))
-    log.info("[fast_insert]: Validating objects after bulk read")
-    for obj in mongo_objs:
-        obj.validate()
-    log.info("[fast_insert]: Validation OK")
-    log.info("[fast_insert]: Done")
-    return mongo_objs
-
-
-def fast_validate_and_insert_with_errors(objects, model, replace=False):
-    try:
-        return fast_validate_and_insert(objects, model, replace)
-    except BulkWriteError as bwe:
-        print(bwe.details)
-        raise RuntimeError()
-
-
-def setup_args():
-    parser = argparse.ArgumentParser(description="Seed the mongo db")
-    parser.add_argument("input", type=str, help="input schedules.json file")
-    parser.add_argument("index", type=str, help="CSV index file with relevant columns")
-    parser.add_argument("ctrlls_data", type=str, help="CSV data of controller models")
-    parser.add_argument("mongo", type=str, help="mongo server url")
-    parser.add_argument("database", type=str, help="mongo database to use")
-    parser.add_argument(
-        "--rebuild",
-        action="store_true",
-        help="drop existing data and rebuild collections",
-    )
-    parser.add_argument(
-        "--extra",
-        action="store_true",
-        help="build and save extra data to the remote db",
-    )
-    parser.add_argument(
-        "--diffdb",
-        action="store_true",
-        help="the remote db will be used for patch generation",
-    )
-    return parser.parse_args()
-
-
-def check_should_continue():
-    global log
-    log.warning("")
-    log.warning("THIS ACTION IS IRREVERSIBLE, ALL EXISTING DATA WILL BE DELETED")
-    log.warning("")
-    log.warning("Should we continue?")
-    shlould_continue = input("[yes/no]: ")
-    if shlould_continue == "yes":
-        return True
-    log.info('Input is not "yes". Aborting operations.')
-    return False
-
-def check_csv_line_valid(line, junc_pattern, otu_pattern):
-    if (
-        line[0]
-        and line[1]
-        and line[2]
-        and junc_pattern.match(line[3])
-        and otu_pattern.match(line[2])
-    ):
-        return True, line[2], line[1]
-    return False, None, None
-
-
 def build_csv_index_item(line, ip_pattern):
     d = {}
     if line[6] and line[7]:
@@ -233,72 +90,11 @@ def build_controller_model_collection(models_csv):
         )
     fast_validate_and_insert(l, ControllerModel)
 
-
-def create_users():
-    l = []
-    acme_corp = ExternalCompany(name="ACME Corporation").save().reload()
-    l.append(
-        User(
-            full_name="DACoT Database Seed",
-            email="seed@dacot.uoct.cl",
-            role="Personal UOCT",
-            area="TIC",
-        )
-    )
-    l.append(
-        User(
-            full_name="Admin",
-            email="admin@dacot.uoct.cl",
-            role="Personal UOCT",
-            area="TIC",
-            is_admin=True,
-        )
-    )
-    l.append(
-        User(
-            full_name="Carlos Ponce",
-            email="carlos.ponce@sansano.usm.cl",
-            role="Personal UOCT",
-            area="TIC",
-            is_admin=True,
-        )
-    )
-    l.append(
-        User(
-            full_name="Sebastian Muñoz",
-            email="sebastian.munozd@sansano.usm.cl",
-            role="Personal UOCT",
-            area="TIC",
-            is_admin=True,
-        )
-    )
-    l.append(
-        User(
-            full_name="Nicolas Grandon",
-            email="ngrandon@uoct.cl",
-            role="Personal UOCT",
-            area="TIC",
-            is_admin=True,
-        )
-    )
-    l.append(
-        User(
-            full_name="ACME Employee",
-            email="employee@acmecorp.com",
-            role="Empresa",
-            area="Mantenedora",
-            company=acme_corp,
-        )
-    )
-    fast_validate_and_insert(l, User)
-
-
 def get_companies_dict():
     comp = {}
     for c in ExternalCompany.objects.all():
         comp[c.name] = c
     return comp
-
 
 def build_project_meta(csv_index):
     r = {}
@@ -399,7 +195,6 @@ def build_junction_plans(junctions, json_data):
         jd[j.jid] = j
     return jd
 
-
 def build_otu_programs(otus, json_data, projects):
     done = set()
     programs = {}
@@ -422,23 +217,3 @@ def build_otu_programs(otus, json_data, projects):
         od[o.oid] = o
     projects = fast_validate_and_insert_with_errors(projects, Project)
     return projects
-
-
-def build_latest_versions():
-    global is_diff
-    lstp = []
-    if is_diff:
-        return
-    for proj in Project.objects.all():
-        proj.metadata.version = "latest"
-        proj.id = None
-        lstp.append(proj)
-    fast_validate_and_insert_with_errors(lstp, Project)
-
-def seed_from_interpreter(uri, db, ctrl, juncs, scheds, communes):
-    build_controller_model_collection(controllers_model_csv)
-    otus, projects = build_projects(index_csv)
-    junctions, otus = build_junctions(index_csv, otus)
-    junctions = build_junction_plans(junctions, json_data)
-    projects = build_otu_programs(otus, json_data, projects)
-    build_latest_versions()
