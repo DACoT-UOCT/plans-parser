a00397a22e1462719f427af1b469b7a9a5705b72
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2021-04-08 21:45:25 -0400

    Use .items to get dict values

--- export-agent/export-agent.py
+++ export-agent/export-agent.py
@@ -12,7 +12,8 @@ class ExportAgent:
         self.__utc_host = env['UTC_HOST']
         self.__utc_user = env['UTC_USER']
         self.__utc_passwd = env['UTC_PASS']
-        self.__read_plans_sleep = 120
+        # self.__read_plans_sleep = 120
+        self.__read_plans_sleep = 5
 
     def run_full_session(self):
         logger.info('Starting FULL SESSION!')
@@ -33,7 +34,7 @@ class ExportAgent:
         logger.info('Saving TCE execution result to {}'.format(outfile))
         with open(outfile, 'w') as out:
             res = executor.get_results()
-            for k, v in res:
+            for k, v in res.items():
                 out.write('{}\n'.format('=' * 30))
                 out.write('{}\n'.format(k))
                 out.write('{}\n'.format('=' * 30))
@@ -42,7 +43,7 @@ class ExportAgent:
     def __get_plans(self, executor):
         logger.info('Building get-plans procedure')
         executor.command('get-plans-wildcard', 'LIPT A000000 TIMINGS')
-        logger.warning('Using a {} (s) sleep call to wait for buffers from remote'.format(self.__read_plans_sleep))
+        logger.warning('Using a {}s sleep call to wait for buffers from remote'.format(self.__read_plans_sleep))
         executor.sleep(self.__read_plans_sleep)
         executor.read_lines(encoding="iso-8859-1", line_ending=b"\x1b8\x1b7")
         logger.debug('Get-Plans built')
